35371f4f85026b0d68d384cc517b14195eee0ba8
harishreddy05 <harishreddy05@gmail.com>
2015-11-12 23:36:55 +0530

    search

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,5 +1,10 @@
 angular.module('saarang2016App')
-  .controller('HeaderCtrl', function ($scope) {
+  .controller('HeaderCtrl', function ($scope,$http) {
   	$scope.isLoggedIn=false;
-
+  	 $http.get('http://erp.saarang.org/api/mobile/display_events/').then(function successCallback(response) {
+    $scope.displayEvents = response.data.data;
+    	console.log($scope.displayEvents);
+  }, function errorCallback(response) {
+    console.log("No Internet Connection");
   });
+});
